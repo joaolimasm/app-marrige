17ce4b94ec5f526d6020863843f2df0cc0fb6f21
joaolimasm <jv.limasm@gmail.com>
2022-11-24 13:14:34 -0300

    initial commit

---
+++ app/data/QuizData.js
@@ -0,0 +1,27 @@
+export default data = [
+    {
+        question: "Qual a data do nosso primeiro beijo?",
+        options: ["04 Dezembro","07 Novembro","04 Janeiro","25 Abril"],
+        correct_option: "04 Dezembro"
+    },
+    {
+        question: "Qual a data do nosso aniversario de namoro?",
+        options: ["07 Dezembro","04 Novembro","04 Janeiro","25 Março"],
+        correct_option: "25 Março"
+    },
+    {
+        question: "Quando foi nosso primeiro date sozinhos?",
+        options: ["05 Abril","07 Dezembro","24 Novembro","13 25 Março"],
+        correct_option: "07 Dezembro"
+    },
+    {
+        question: "Qual foi nossa primeira viagem juntos?",
+        options: ["Rio de Janeiro","Caldas Novas","Joao Pessoa","Chile"],
+        correct_option: "Caldas Novas"
+    },
+    {
+        question: "A pergunta que vale um milhao de reais: Voce aceita casar comigo?",
+        options: ["Sim","Talvez","Nao","Num sei"],
+        correct_option: "Sim"
+    }
+]
